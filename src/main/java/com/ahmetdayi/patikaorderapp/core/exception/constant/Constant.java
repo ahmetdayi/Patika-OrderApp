825428bd22b40d14e2cd097cd113df1c8db3d80a
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-04 16:26:42 +0300

    Exception constant class created

---
+++ src/main/java/com/ahmetdayi/patikaorderapp/core/exception/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.ahmetdayi.patikaorderapp.core.exception.constant;
+
+public class Constant {
+    public static final String USER_NOT_FOUND = "User Not Found.";
+    public static final String COMPANY_NOT_FOUND = "Company Not Found.";
+}
+
