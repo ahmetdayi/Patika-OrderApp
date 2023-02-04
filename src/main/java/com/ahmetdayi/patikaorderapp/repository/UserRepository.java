864ef5d0c59c6f77fbf0379997f9c20f06ff7126
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-04 18:33:48 +0300

    fixed user repository

--- src/main/java/com/ahmetdayi/patikaorderapp/repository/UserRepository.java
+++ src/main/java/com/ahmetdayi/patikaorderapp/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.ahmetdayi.patikaorderapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
