08caf2bc7b206dc91fe75b468aa5504fa70aeb20
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-02-04 13:33:57 +0300

    OrderApp project created

---
+++ src/main/java/com/ahmetdayi/patikaorderapp/PatikaOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetdayi.patikaorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaOrderAppApplication.class, args);
+	}
+
+}
